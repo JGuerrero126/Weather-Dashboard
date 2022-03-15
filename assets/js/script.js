0ca389fe849f0218504b1bb951cbb36f0cbe0a29
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-14 20:47:52 -0700

    made the search bar and the area to display the current day, also more work in the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
+var cityNameDate = $("#cityNameDate")
+var temp = $("#temp")
+var wind = $("#wind")
+var humidity = $("#humidity") 
+var uv = $("#uv")
+var cityInput = $("#cityInput")
+
+
+
 function weatherInfo() {
-    var userRequest = localStorage.getItem("userInput");
     var locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + "london" + "&limit=1&appid=23343143cc261b0ecb35b3be3eb91e01"
   
     fetch(locationURL)
@@ -12,11 +20,13 @@ function weatherInfo() {
       })
       .then(function (data) {
         console.log(data);
+        var rightNow = moment().format("(M/D/YYYY)")
+        cityNameDate.text(data[0].name + " " + rightNow)
         var requestedLat = data[0].lat
         var requestedLon = data[0].lon
         console.log(requestedLat)
         console.log(requestedLon)
-        var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + requestedLat + "&lon=" + requestedLon + "&exclude=minutely,hourly&appid=23343143cc261b0ecb35b3be3eb91e01"
+        var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + requestedLat + "&lon=" + requestedLon + "&exclude=minutely,hourly&units=imperial&appid=23343143cc261b0ecb35b3be3eb91e01"
     console.log(weatherURL)
     fetch(weatherURL)
     .then(function (response) {
@@ -28,25 +38,11 @@ function weatherInfo() {
     })
     .then(function (data) {
       console.log(data);
-
-      for (i = 0; i < 5; i++) {
-        var date = moment(data[i].datetime).format("MMM Do, YYYY");
-        console.log(moment(date).format("MMM Do, YYYY"));
-        var li = $("<li>");
-        $("<a>")
-          .attr("href", data[i].url)
-          .text(
-            data[0].artist.name +
-              " Performing At " +
-              data[i].venue.name +
-              " " +
-              data[i].venue.location +
-              " " +
-              date
-          )
-          .appendTo(li);
-        li.appendTo(concertList);
-      }
+      temp.text("Temp: " + data.current.temp)
+      wind.text("Wind: " + data.current.wind_speed)
+      humidity.text("Humidity: " + data.current.humidity)
+      uv.text("UV Index: " + data.current.uvi)
+      
     });
     });
 }
