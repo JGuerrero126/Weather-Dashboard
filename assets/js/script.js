a98cd194f742ed0517b210173411a59f1a702e7a
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-17 00:09:59 -0700

    made all the icons work

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,27 @@ var wind = $("#wind");
 var humidity = $("#humidity");
 var uv = $("#uv");
 var oneDate = $("#oneDayDate");
+var icon1 = $("#wicon1")
 var oneTemp = $("#oneDayTemp");
 var oneWind = $("#oneDayWind");
 var oneHumidity = $("#oneDayHumid");
 var twoDate = $("#twoDayDate");
+var icon2 = $("#wicon2")
 var twoTemp = $("#twoDayTemp");
 var twoWind = $("#twoDayWind");
 var twoHumidity = $("#twoDayHumid");
 var threeDate = $("#threeDayDate");
+var icon3 = $("#wicon3")
 var threeTemp = $("#threeDayTemp");
 var threeWind = $("#threeDayWind");
 var threeHumidity = $("#threeDayHumid");
 var fourDate = $("#fourthDayDate");
+var icon4 = $("#wicon4")
 var fourTemp = $("#fourthDayTemp");
 var fourWind = $("#fourthDayWind");
 var fourHumidity = $("#fourthDayHumid");
 var fiveDate = $("#fiveDayDate");
+var icon5 = $("#wicon5")
 var fiveTemp = $("#fiveDayTemp");
 var fiveWind = $("#fiveDayWind");
 var fiveHumidity = $("#fiveDayHumid");
@@ -62,9 +67,19 @@ function weatherInfo() {
     })
     .then(function (data) {
       console.log(data);
-      temp.text("Temp: " + data.current.temp)
-      wind.text("Wind: " + data.current.wind_speed)
-      humidity.text("Humidity: " + data.current.humidity)
+      var iconcode1 = data.daily[0].weather[0].icon;
+      var iconcode2 = data.daily[1].weather[0].icon;
+      var iconcode3 = data.daily[2].weather[0].icon;
+      var iconcode4 = data.daily[3].weather[0].icon;
+      var iconcode5 = data.daily[4].weather[0].icon;
+      var iconurl1 = "http://openweathermap.org/img/w/" + iconcode1 + ".png";
+      var iconurl2 = "http://openweathermap.org/img/w/" + iconcode2 + ".png";
+      var iconurl3 = "http://openweathermap.org/img/w/" + iconcode3 + ".png";
+      var iconurl4 = "http://openweathermap.org/img/w/" + iconcode4 + ".png";
+      var iconurl5 = "http://openweathermap.org/img/w/" + iconcode5 + ".png";
+      temp.text("Temp: " + data.current.temp + "°F")
+      wind.text("Wind: " + data.current.wind_speed + " MPH")
+      humidity.text("Humidity: " + data.current.humidity + " %")
       if (data.current.uvi < 2) {
         uv.text("UV Index: " + data.current.uvi).addClass("greenback")
       } else if (data.current.uvi < 5) {
@@ -78,25 +93,30 @@ function weatherInfo() {
       }
       time = moment().add(i+1, "d").format("(M/D/YYYY)")
       oneDate.text(moment().add(1, "d").format("(M/D/YYYY)"))
-      oneTemp.text("Temp: " + data.daily[0].temp.day)
-      oneWind.text("Wind: " + data.daily[0].wind_speed)
-      oneHumidity.text("Humidity: " + data.daily[0].humidity)
+      icon1.attr("src", iconurl1)
+      oneTemp.text("Temp: " + data.daily[0].temp.day + "°F")
+      oneWind.text("Wind: " + data.daily[0].wind_speed + " MPH")
+      oneHumidity.text("Humidity: " + data.daily[0].humidity + " %")
       twoDate.text(moment().add(2, "d").format("(M/D/YYYY)"))
-      twoTemp.text("Temp: " + data.daily[1].temp.day)
-      twoWind.text("Wind: " + data.daily[1].wind_speed)
-      twoHumidity.text("Humidity: " + data.daily[1].humidity)
+      icon2.attr("src", iconurl2)
+      twoTemp.text("Temp: " + data.daily[1].temp.day + "°F")
+      twoWind.text("Wind: " + data.daily[1].wind_speed + " MPH")
+      twoHumidity.text("Humidity: " + data.daily[1].humidity + " %")
       threeDate.text(moment().add(3, "d").format("(M/D/YYYY)"))
-      threeTemp.text("Temp: " + data.daily[2].temp.day)
-      threeWind.text("Wind: " + data.daily[2].wind_speed)
-      threeHumidity.text("Humidity: " + data.daily[2].humidity)
+      icon3.attr("src", iconurl3)
+      threeTemp.text("Temp: " + data.daily[2].temp.day + "°F")
+      threeWind.text("Wind: " + data.daily[2].wind_speed + " MPH")
+      threeHumidity.text("Humidity: " + data.daily[2].humidity + " %")
       fourDate.text(moment().add(4, "d").format("(M/D/YYYY)"))
-      fourTemp.text("Temp: " + data.daily[3].temp.day)
-      fourWind.text("Wind: " + data.daily[3].wind_speed)
-      fourHumidity.text("Humidity: " + data.daily[3].humidity)
+      icon4.attr("src", iconurl4)
+      fourTemp.text("Temp: " + data.daily[3].temp.day + "°F")
+      fourWind.text("Wind: " + data.daily[3].wind_speed + " MPH")
+      fourHumidity.text("Humidity: " + data.daily[3].humidity + " %")
       fiveDate.text(moment().add(5, "d").format("(M/D/YYYY)"))
-      fiveTemp.text("Temp: " + data.daily[4].temp.day)
-      fiveWind.text("Wind: " + data.daily[4].wind_speed)
-      fiveHumidity.text("Humidity: " + data.daily[4].humidity)
+      icon5.attr("src", iconurl5)
+      fiveTemp.text("Temp: " + data.daily[4].temp.day + "°F")
+      fiveWind.text("Wind: " + data.daily[4].wind_speed + " MPH")
+      fiveHumidity.text("Humidity: " + data.daily[4].humidity + " %")
     })
   })
 }
@@ -170,9 +190,19 @@ pastSearches.on("click", function (event) {
     })
     .then(function (data) {
       console.log(data);
-      temp.text("Temp: " + data.current.temp)
-      wind.text("Wind: " + data.current.wind_speed)
-      humidity.text("Humidity: " + data.current.humidity)
+      var iconcode1 = data.daily[0].weather[0].icon;
+      var iconcode2 = data.daily[1].weather[0].icon;
+      var iconcode3 = data.daily[2].weather[0].icon;
+      var iconcode4 = data.daily[3].weather[0].icon;
+      var iconcode5 = data.daily[4].weather[0].icon;
+      var iconurl1 = "http://openweathermap.org/img/w/" + iconcode1 + ".png";
+      var iconurl2 = "http://openweathermap.org/img/w/" + iconcode2 + ".png";
+      var iconurl3 = "http://openweathermap.org/img/w/" + iconcode3 + ".png";
+      var iconurl4 = "http://openweathermap.org/img/w/" + iconcode4 + ".png";
+      var iconurl5 = "http://openweathermap.org/img/w/" + iconcode5 + ".png";
+      temp.text("Temp: " + data.current.temp + "°F")
+      wind.text("Wind: " + data.current.wind_speed + " MPH")
+      humidity.text("Humidity: " + data.current.humidity + " %")
       if (data.current.uvi < 2) {
         uv.text("UV Index: " + data.current.uvi).addClass("greenback")
       } else if (data.current.uvi < 5) {
@@ -186,25 +216,30 @@ pastSearches.on("click", function (event) {
       }
       time = moment().add(i+1, "d").format("(M/D/YYYY)")
       oneDate.text(moment().add(1, "d").format("(M/D/YYYY)"))
-      oneTemp.text("Temp: " + data.daily[0].temp.day)
-      oneWind.text("Wind: " + data.daily[0].wind_speed)
-      oneHumidity.text("Humidity: " + data.daily[0].humidity)
+      icon1.attr("src", iconurl1)
+      oneTemp.text("Temp: " + data.daily[0].temp.day + "°F")
+      oneWind.text("Wind: " + data.daily[0].wind_speed + " MPH")
+      oneHumidity.text("Humidity: " + data.daily[0].humidity + " %")
       twoDate.text(moment().add(2, "d").format("(M/D/YYYY)"))
-      twoTemp.text("Temp: " + data.daily[1].temp.day)
-      twoWind.text("Wind: " + data.daily[1].wind_speed)
-      twoHumidity.text("Humidity: " + data.daily[1].humidity)
+      icon2.attr("src", iconurl2)
+      twoTemp.text("Temp: " + data.daily[1].temp.day + "°F")
+      twoWind.text("Wind: " + data.daily[1].wind_speed + " MPH")
+      twoHumidity.text("Humidity: " + data.daily[1].humidity + " %")
       threeDate.text(moment().add(3, "d").format("(M/D/YYYY)"))
-      threeTemp.text("Temp: " + data.daily[2].temp.day)
-      threeWind.text("Wind: " + data.daily[2].wind_speed)
-      threeHumidity.text("Humidity: " + data.daily[2].humidity)
+      icon3.attr("src", iconurl3)
+      threeTemp.text("Temp: " + data.daily[2].temp.day + "°F")
+      threeWind.text("Wind: " + data.daily[2].wind_speed + " MPH")
+      threeHumidity.text("Humidity: " + data.daily[2].humidity + " %")
       fourDate.text(moment().add(4, "d").format("(M/D/YYYY)"))
-      fourTemp.text("Temp: " + data.daily[3].temp.day)
-      fourWind.text("Wind: " + data.daily[3].wind_speed)
-      fourHumidity.text("Humidity: " + data.daily[3].humidity)
+      icon4.attr("src", iconurl4)
+      fourTemp.text("Temp: " + data.daily[3].temp.day + "°F")
+      fourWind.text("Wind: " + data.daily[3].wind_speed + " MPH")
+      fourHumidity.text("Humidity: " + data.daily[3].humidity + " %")
       fiveDate.text(moment().add(5, "d").format("(M/D/YYYY)"))
-      fiveTemp.text("Temp: " + data.daily[4].temp.day)
-      fiveWind.text("Wind: " + data.daily[4].wind_speed)
-      fiveHumidity.text("Humidity: " + data.daily[4].humidity)
+      icon5.attr("src", iconurl5)
+      fiveTemp.text("Temp: " + data.daily[4].temp.day + "°F")
+      fiveWind.text("Wind: " + data.daily[4].wind_speed + " MPH")
+      fiveHumidity.text("Humidity: " + data.daily[4].humidity + " %")
     })
   })
 }
